642f5156d0c85f6cf40f4d485d35435334c6610d
fulh <fulh@163.com>
2022-06-22 16:41:03 +0800

    基本信息

--- apps/interface/views.py
+++ apps/interface/views.py
@@ -1,3 +1,24 @@
+import demjson
+import requests
 from django.shortcuts import render
+from .models import InterfaceInfo
 
-# Create your views here.
+def test_case(request):
+	nid = [1]
+	# nid=request.GET.get('nid')
+	Interfaceobj=InterfaceInfo.objects.get(id__in=nid)
+	# for a in Interfaceobj:
+	# 	print(a.case_name)
+	print(Interfaceobj.case_group)
+	response = requests.request(
+		Interfaceobj.request_mode,
+		Interfaceobj.interface_url,
+		data=Interfaceobj.request_body,
+		headers=demjson.decode(Interfaceobj.request_head),
+		params=demjson.decode(Interfaceobj.request_parameter)
+	)
+	print(response.status_code)
+	# 实际的响应代码
+	print(response.text)
+	# 实际的响应文本
+	return render(request,"index.html")
